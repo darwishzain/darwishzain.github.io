5e57a96e79de1745a24e0154703131867be1a3fe
Darwish Mat Zain <62579706+darwishzain@users.noreply.github.com>
2022-02-22 19:22:27 +0800

    Update file management 3

---
+++ static/includes/head.php
@@ -0,0 +1,7 @@
+<meta charset="UTF-8">
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<title>Darwish Mat Zain</title>
+<script src='https://kit.fontawesome.com/6d1cef488a.js' crossorigin='anonymous'></script>
+<link rel='icon' href='./static/image/dzslogo.png'>
+<link rel='stylesheet' href='./static/styles/main.css'>
