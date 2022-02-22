67fd95f6e3322ef0a370162d6bd1496a232c7372
Darwish Mat Zain <62579706+darwishzain@users.noreply.github.com>
2022-02-22 19:18:40 +0800

    Update file management 1

--- include/head.php
+++
@@ -1,3 +0,0 @@
-<script src='https://kit.fontawesome.com/6d1cef488a.js' crossorigin='anonymous'></script>
-<link rel='icon' href='./img/dzslogo.png'>
-<link rel='stylesheet' href='./styles/main.css'>
