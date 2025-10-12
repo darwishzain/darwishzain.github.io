40c5df25f746ae37b9f220ff4b787af49b884550
Darwish Mat Zain - Hidari Shoya <darwishzainstudio@gmail.com>
2025-10-13 01:33:40 +0800

    update link aggregator w/ image and icon

--- link-aggregator/main.js
+++ link-aggregator/main.js
@@ -19,27 +19,45 @@ document.addEventListener('DOMContentLoaded', function() {
                 favicon.href = data.icon;
             }
 
-
-            for (const [name, url] of Object.entries(data.links)) {
-                const link = document.createElement('a');
-                link.href = url;
-                link.target = '_blank';
-                link.title = name;
-                link.textContent = name;
-                link.className = 'btn btn-primary w-100';
-                link.style.maxWidth = '400px';
-                links.appendChild(link);
-            }
             for (const [name, url] of Object.entries(data.socials)) {
                 const social = document.createElement('a');
                 social.href = url;
                 social.target = '_blank';
                 social.title = name;
-                social.textContent = name.charAt(0).toUpperCase() + name.slice(1);
-                social.className = 'btn btn-outline-secondary btn-light btn-sm';
+                social.textContent = name;
+                social.className = 'btn btn-dark btn-sm';
                 socials.appendChild(social);
             }
-            document.getElementById('footer');
+            for (const [key, value] of Object.entries(data.links)) {
+                const link = document.createElement('a');
+                link.href = value.url;
+                link.target = '_blank';
+                //link.textContent = value.name;
+                link.style = 'display: flex;flex-direction: column;align-items: center;text-align: center;width: 100%;';
+                link.className = 'link btn btn-light';
+                if(value.img && value.img.trim() !== "") {
+                    link.innerHTML = '<img src="'+value.img+'" alt="'+value.name+'" class="link-img" style="display: block;margin-bottom: 10px;height: 50vh;max-width: 100%;">';
+                    //const img = document.createElement('img');
+                    //img.src = value.img;
+                    //img.alt = value.name;
+                    //img.className = 'link-img';
+                    //img.style = 'display: block;margin-bottom: 10px;height: 50vh;width: auto;';
+                    //link.appendChild(img);
+                }
+                const textspan = document.createElement('span');
+                textspan.style = 'text-decoration:none;';
+                if(value.icon && value.icon.trim() !== "") {
+                    textspan.innerHTML = '<img src="'+value.icon+'" alt="'+value.name+'" style="height:10vh;"> '+value.name;
+                }
+                else
+                {
+                    textspan.textContent = value.name;
+                }
+                link.appendChild(textspan);
+
+                links.appendChild(link);
+            }
+            //document.getElementById('footer');
         })
         .catch(error => {
             console.error('Error fetching config:', error);
