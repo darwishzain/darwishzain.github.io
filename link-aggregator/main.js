5a2b8fe97c02570dcf21bd16d6cf854b0aff4569
Darwish Mat Zain - Hidari Shoya <darwishzainstudio@gmail.com>
2025-10-25 10:30:17 +0800

    new

--- link-aggregator/main.js
+++ link-aggregator/main.js
@@ -2,13 +2,11 @@ config = 'config.json';
 document.addEventListener('DOMContentLoaded', function() {
     const links = document.getElementById('links');
     const socials = document.getElementById('socials');
+    const username = document.getElementById('username');
     fetch(config)
         .then(response => response.json())
         .then(data => {
-            document.title = '@'+data.username;
-            document.body.style.background = data.backgroundcolor;
-            document.getElementById('title').textContent = data.name;
-            document.getElementById('username').textContent = '@'+data.username;
+            document.title = `@${data.username}`;
             if (data.icon && data.icon.trim() !== "") {
                 let favicon = document.querySelector('link[rel="icon"]');
                 if (!favicon) {
@@ -18,25 +16,36 @@ document.addEventListener('DOMContentLoaded', function() {
                 }
                 favicon.href = data.icon;
             }
-
+            document.body.classList.toggle(`la-${data.theme}`);
+            //document.getElementById('title').textContent = data.name;
+            if(data.profile != '')
+            {
+                username.innerHTML = `<img src="${data.profile}" alt="" id="profile" srcset=""><br>@${data.username}`;
+            }
+            else
+            {
+                username.innerHTML = `@${data.username}`;
+            }
+            socials.classList.add(`la-${data.theme}`);
             for (const [name, url] of Object.entries(data.socials)) {
                 const social = document.createElement('a');
                 social.href = url;
                 social.target = '_blank';
                 social.title = name;
                 social.textContent = name;
-                social.className = 'btn btn-dark btn-sm';
+                social.className = 'la-btn';
                 socials.appendChild(social);
             }
+            links.classList.add(`la-${data.theme}`);
             for (const [key, value] of Object.entries(data.links)) {
                 const link = document.createElement('a');
                 link.href = value.url;
                 link.target = '_blank';
                 //link.textContent = value.name;
-                link.style = 'display: flex;flex-direction: column;align-items: center;text-align: center;width: 100%;';
-                link.className = 'link btn btn-light';
+                //link.style = 'display: flex;flex-direction: column;align-items: center;text-align: center;width: 100%;';
+                link.className = 'links';
                 if(value.img && value.img.trim() !== "") {
-                    link.innerHTML = '<img src="'+value.img+'" alt="'+value.name+'" class="link-img" style="display: block;margin-bottom: 10px;height: 50vh;max-width: 100%;">';
+                    link.innerHTML = `<img src="${value.img}" alt="${value.name}" class="link-img" style="display: block;margin-bottom: 10px;height: 50vh;max-width: 100%;">`;
                     //const img = document.createElement('img');
                     //img.src = value.img;
                     //img.alt = value.name;
